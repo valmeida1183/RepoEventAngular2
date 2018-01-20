b522056f8721a57f732fd9272a6b1a59d08813d7
Vinicius Lopes de Almeida <termobin@gmail.com>
2018-01-20 02:18:12 -0200

    Aula 22

--- NgBasico/NgTemplate/src/app/app.module.ts
+++ NgBasico/NgTemplate/src/app/app.module.ts
@@ -1,29 +1,53 @@
 // Importação das dependências
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
 
+// Importação dos componentes do bootstrap
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
+// Importação dos componentes shared
+import { MenuSuperiorComponent } from './shared/menu-superior/menu-superior.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainPrincipalComponent } from './shared/main-principal/main-principal.component';
+import { MenuLoginComponent } from './shared/menu-login/menu-login.component';
 
 // Importação dos componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListaEventosComponent } from './eventos/lista-eventos/lista-eventos.component';
 
+// Importação dos Serviços
+import { SeoService } from './services/seo.service';
 
 @NgModule({
   declarations: [
-    // Aqui vão as minhas dependêcias, como components, services etc..
-    AppComponent
+    // Aqui vão as minhas dependêcias, como components.
+    AppComponent,
+    MenuSuperiorComponent,
+    FooterComponent,
+    MainPrincipalComponent,
+    HomeComponent,
+    MenuLoginComponent,
+    ListaEventosComponent
   ],
   imports: [
-    // Aqui vão as dependências do próprio Angular.
+    // Aqui vão as importações dos módulos.
     BrowserModule,
     FormsModule,
     HttpModule,
-    CollapseModule.forRoot() // Alguns módulos precisam chamar esse método para entender que são chamados pelo módulo raiz.
+    CollapseModule.forRoot(), // Alguns módulos precisam chamar esse método para entender que são chamados pelo módulo raiz.
+    CarouselModule.forRoot(),
+    RouterModule.forRoot(rootRouterConfig, {useHash: false})
   ],
   providers: [
     // Aqui são outros tipos de provedores.
+    Title,
+    SeoService
   ],
   bootstrap: [
     // Aqui eu declaro o componente que irá inicializar a aplicação.
---
+++ NgBasico/NgTemplate/src/app/eventos/lista-eventos/lista-eventos.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { SeoService, SeoModel } from '../../services/seo.service';
+
+@Component({
+  selector: 'app-lista-eventos',
+  templateUrl: './lista-eventos.component.html',
+  styleUrls: ['./lista-eventos.component.css']
+})
+export class ListaEventosComponent implements OnInit {
+
+  constructor(seoService: SeoService) {
+    // tslint:disable-next-line:prefer-const
+    let seoModel: SeoModel = <SeoModel>{
+      title: 'Próximos Eventos',
+      description: 'Lista dos próximos eventos técnicos no Brasil',
+      robots: 'Index, Follow',
+      keywords: 'eventos,workshops,encontros,congressos'
+    };
+
+    seoService.setSeoData(seoModel);
+  }
+
+  ngOnInit() {
+  }
+
+}
